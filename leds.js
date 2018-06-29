5e8433674914e17d0ced21e8ddbdff09a7540b3a
Kunigumi Yen <kuni93@users.noreply.github.com>
2018-06-29 19:32:57 +0800

    leds.js 新增同步功能、詳細註解

--- leds.js
+++ leds.js
@@ -1,44 +1,54 @@
-const http = require("http")
-const queryString = require("querystring")
-const gpio = require("onoff").Gpio
+// 引入相關套件、函式庫
+// 其中 onoff 無內建請先使用 `npm install onoff --save` 安裝
+const http = require('http')
+const queryString = require('querystring')
+const gpio = require('onoff').Gpio
 
+// 定義要使用的 GPIO
+const LED0 = new gpio(26, 'out')
+const LED1 = new gpio(19, 'out')
+const LED2 = new gpio(13, 'out')
+const LED3 = new gpio( 6, 'out')
+const LED4 = new gpio( 5, 'out')
+const LED5 = new gpio(11, 'out')
+const LED6 = new gpio( 9, 'out')
+const LED7 = new gpio(10, 'out')
+const LED8 = new gpio(22, 'out')
+const LED9 = new gpio(27, 'out')
+const LEDs = [LED0, LED1, LED2, LED3, LED4, LED5, LED6, LED7, LED8, LED9]
+
+// 定義及初始化模式、顯示數字、同步
 const rangeMode = /^[0-2]+$/
 const rangeDigi = /^\d$/
 
-const LED0 = new gpio(26, 'out');
-const LED1 = new gpio(19, 'out');
-const LED2 = new gpio(13, 'out');
-const LED3 = new gpio( 6, 'out');
-const LED4 = new gpio( 5, 'out');
-const LED5 = new gpio(11, 'out');
-const LED6 = new gpio( 9, 'out');
-const LED7 = new gpio(10, 'out');
-const LED8 = new gpio(22, 'out');
-const LED9 = new gpio(27, 'out');
-const LEDs = [LED0, LED1, LED2, LED3, LED4, LED5, LED6, LED7, LED8, LED9];
-
-let gpioDatas = {
+let gpioData = {
   mode: 0,
   digi: 0,
   sync: false
 }
 
+// 定義同步計時器及目標位置
+let syncInterval
+let syncSource = 'http://{{IP}}:{{PORT}}}/{{PATH}}'
+
+// 每 40ms 更新一次 LEDs 電路
 function ledUpdate() {
-  if (gpioDatas.mode === 0) {
+  if (gpioData.mode === 0) {
     LEDs.forEach(function(currentValue) {
       currentValue.writeSync(1)
     })
-  } else if (gpioDatas.mode === 1) {
-    ledNumber(gpioDatas.digi)
-  } else if (gpioDatas.mode === 2) {
+  } else if (gpioData.mode === 1) {
+    ledNumber(gpioData.digi)
+  } else if (gpioData.mode === 2) {
     ledLoading()
   } else {
-    gpioDatas.mode = 0
+    gpioData.mode = 0
   }
 }
 
 setInterval(ledUpdate, 40)
 
+// 輸出到 LEDs 電路
 function ledNumber(number) {
   LEDs.forEach(function(currentValue) {
     if (number + 1 > 0) {
@@ -50,57 +60,79 @@ function ledNumber(number) {
   })
 }
 
+// 輸出動畫到 LEDs 電路
 function ledLoading() {
   LEDs.forEach(function(currentValue) {
     currentValue.writeSync(1)
   })
-  LEDs[gpioDatas.digi].writeSync(0)
-  if (gpioDatas.digi >= 9) {
-    gpioDatas.digi = 0
+  LEDs[gpioData.digi].writeSync(0)
+  if (gpioData.digi >= 9) {
+    gpioData.digi = 0
   } else {
-    ++ gpioDatas.digi
+    ++ gpioData.digi
   }
 }
 
+// 用 HTTP GET 取得目標來源的狀態
+function getSync() {
+  http.get(syncSource, (res) => {
+    let syncData = ''
+
+    res.on('data', (chunk) => {
+      syncData += chunk
+    })
+
+    res.on('end', () => {
+      gpioData.mode = JSON.parse(syncData).mode // 解析 JSON 格式
+      gpioData.digi = JSON.parse(syncData).digi // 解析 JSON 格式
+    })
+
+  }).on('error', (err) => {
+    console.log('Error: ' + err.message)
+  })
+}
+
 requestHandler = (request, response) => {
-  console.log(request.method + " Request: " + request.url)
+  console.log(request.method + ' Request: ' + request.url)
 
-  if (request.url === "/get" && request.method === "GET") {
+  if (request.url === '/get' && request.method === 'GET') {
 
-    response.setHeader("Content-Type", "application/json")
-    response.end(JSON.stringify(gpioDatas))
+    response.setHeader('Content-Type', 'application/json')
+    response.end(JSON.stringify(gpioData))
 
-  } else if (request.url === "/set" && request.method === "POST") {
+  } else if (request.url === '/set' && request.method === 'POST') {
 
-    let getDatas = [];
+    let getData = []
     request.on('data', (chunk) => {
-      getDatas.push(chunk)
+      getData.push(chunk)
     }).on('end', () => {
-      getDatas = queryString.parse(getDatas.toLocaleString());
-      console.log("POST: ", getDatas);
+      getData = queryString.parse(getData.toLocaleString())
+      console.log('POST: ', getData)
 
-      if (rangeMode.test(getDatas.mode)) {
-        gpioDatas.mode = parseInt(getDatas.mode)
+      if (rangeMode.test(getData.mode)) { // 用正規表示式檢查傳入的參數
+        gpioData.mode = parseInt(getData.mode)
       }
 
-      if (rangeDigi.test(getDatas.digi)) {
-        gpioDatas.digi = parseInt(getDatas.digi)
+      if (rangeDigi.test(getData.digi)) { // 用正規表示式檢查傳入的參數
+        gpioData.digi = parseInt(getData.digi)
       }
 
-      if (getDatas.sync === "true") {
-        gpioDatas.sync = true
-      } else if (getDatas.sync === "false") {
-        gpioDatas.sync = false
+      if (getData.sync === 'true') {
+        gpioData.sync = true
+        syncInterval = setInterval(getSync, 500)
+      } else if (getData.sync === 'false') {
+        gpioData.sync = false
+        clearInterval(syncInterval)
       }
 
-      response.setHeader("Content-Type", "application/json")
-      response.end(JSON.stringify(gpioDatas))
+      response.setHeader('Content-Type', 'application/json')
+      response.end(JSON.stringify(gpioData))
     })
 
   } else {
-    response.end("Use HTTP GET method request '/get' or POST method request '/set'.");
+    response.end('Use HTTP GET method request `/get` or POST method request `/set`.')
   }
 }
 
 const server = http.createServer(requestHandler)
-server.listen(3000)
+server.listen(3000) // 啟動 HTTP Server
